74719c7b2382d50c3ab360b1087dcc87acd17af2
kimjinu <kimjinu5555@gmail.com>
2017-09-23 13:11:18 +0900

    port to 8080
    old: 80
    new: 8080

--- WebandServer/server.js
+++ WebandServer/server.js
@@ -20,7 +20,7 @@ app.use(express.static('Html'));
 
 var router = require('./routes/Web/webIndex')(app, target);
 
-var port = process.env.PORT || 80;
+var port = process.env.PORT || 8080;
 var server = app.listen(port, function(){
   console.log("Server is running...");
   console.log("10.80.161.30");
