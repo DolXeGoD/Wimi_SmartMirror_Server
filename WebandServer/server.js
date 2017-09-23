674449fec36db1aaaba2a9462dc0cf7dca81aa71
kimjinu <kimjinu5555@gmail.com>
2017-09-23 11:56:47 +0900

    Server add Html
    Server add Html

--- WebandServer/server.js
+++ WebandServer/server.js
@@ -16,6 +16,7 @@ db.once('open', function(){
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(express.static('Html'));
 
 var router = require('./routes')(app, target);
 
